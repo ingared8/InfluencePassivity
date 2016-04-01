5ae58d4523b2a18879be63dc1bbf441a1aee8114
Ganga Reddy <gangareddy.ts@gmail.com>
2016-04-01 17:54:43 -0400

    Added a script for forming a Graph for Newyork Street data and Underground data

--- A1/Scripts/ReadDbf.py
+++ A1/Scripts/ReadDbf.py
@@ -5,10 +5,12 @@ import os
 dir  = "/home/ingared/Documents/NS_IP/UndergroundData"
 streets = "LONDON_STREETS.dbf"
 underground = "LONDON_UNDERGROUND.dbf"
+ny_underground = "NY_UNDERGROUND.dbf"
+ny_streets = "NY_STREETS.dbf"
 count = 0
-test1 = DBF(os.path.join(dir,streets))
+test1 = DBF(os.path.join(dir,ny_streets))
 for record in test1:
-    if (count <= 100):
+    if (count <= 10):
         print record
     count += 1
 
@@ -17,7 +19,9 @@ print " Total Edges in Streets  : " , count
 print '\n'
 
 count = 0
-test2 = DBF(os.path.join(dir,underground))
+b = {}
+
+test2 = DBF(os.path.join(dir,ny_underground))
 for record in test2:
     if (count%100 == 0):
         print record
@@ -26,3 +30,4 @@ for record in test2:
 print '\n'
 print " Total Edges in Underground Metro : " , count
 print '\n'
+
--- A1/Scripts/ReadGraphFile.py
+++ A1/Scripts/ReadGraphFile.py
@@ -4,6 +4,8 @@ import networkx as nx
 import os
 import numpy as np
 import re
+import dbf
+from dbfread import DBF
 
 # Common functions
 def splitLine (line):
@@ -40,11 +42,100 @@ def readGraphfromFile(filename, delimiter = "#", weight = False, directional = T
                     weight_value = vals[2]
                 else:
                     weight_value = 1
-                g.add_weighted_edges_from([(vals[0],vals[1],weight_value)], color = 'red')
+                g.add_weighted_edges_from([(vals[0],vals[1])],weight=weight_value, color = 'red')
             linecount += 1
             if ( linecount%100 == 0):
                 print "Reading line no: ",linecount
     f.close()
     print "File Reading Completed"
     return g
+
+
+def get_NY_StreetsGraph():
+
+    dir  = "/home/ingared/Documents/NS_IP/UndergroundData"
+    ny_streets = "NY_STREETS.dbf"
+
+    g = nx.Graph()
+
+
+    count = 0
+    test1 = DBF(os.path.join(dir,ny_streets))
+    print " Started reading NY Street data\n"
+
+    for record in test1:
+
+        edge_id = record['edge_ID']
+        fn_id = record['FN_ID']
+        tn_id = record['TN_ID']
+        length = record['length']
+
+        #print fn_id,tn_id,length, edge_id
+
+        if ( not g.has_node(fn_id)):
+            g.add_node(fn_id)
+        if (not g.has_node(tn_id)):
+            g.add_node(fn_id)
+        g.add_weighted_edges_from([(fn_id,tn_id,length)],edge_id=edge_id)
+
+        count += 1
+        if (count%1000 == 0):
+            print "No of edges read :", count
+
+    print '\n'
+    print " Total Edges in Streets  : " , count
+    print '\n'
+
+    return g
+
+
+def get_NY_UndergoundGraph():
+
+    dir  = "/home/ingared/Documents/NS_IP/UndergroundData"
+    ny_streets = "NY_UNDERGROUND.dbf"
+
+    g = nx.Graph()
+
+    count = 0
+    test1 = DBF(os.path.join(dir,ny_streets))
+    print " Started reading NY Underground data\n"
+
+    for record in test1:
+        edge_id = record['edge_ID']
+        fn_id = record['FN_ID']
+        tn_id = record['TN_ID']
+        length = record['length']
+        fn_x = record['FN_x']
+        fn_y = record['FN_y']
+        tn_x = record['TN_x']
+        tn_y = record["TN_y"]
+        name = record['name']
+        type = record['type']
+
+        #print fn_id,tn_id,length, edge_id
+        if ( not g.has_node(fn_id)):
+            g.add_node(fn_id)
+        if (not g.has_node(tn_id)):
+            g.add_node(fn_id)
+        g.add_weighted_edges_from([(fn_id,tn_id,length)],edge_id=edge_id,fn_x=fn_x,fn_y=fn_y,
+                                  tn_x = tn_x,tn_y = tn_y,name = name,type= type)
+
+        count += 1
+        if (count%1000 == 0):
+            print record
+            print "No of edges read :", count
+
+    print '\n'
+    print " Total Edges in Streets  : " , count
+    print '\n'
+    return g
+
+g1 = get_NY_StreetsGraph()
+g2 = get_NY_UndergoundGraph()
+
+print " Street data from 1 to 2"
+#g1.get_edge_data(1,2)
+
+print " Underground metro from 1 to 2"
+print g2.get_edge_data(1,2)
 
